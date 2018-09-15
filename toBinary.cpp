9c4e0db3d6859ae27a1ae8bd67b59c5f9b088eba
Jennifer Jones <jenniferalexisjones@gmail.com>
2018-09-14 20:29:16 -0400

    got it working

--- toBinary.cpp
+++ toBinary.cpp
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
- * File:   main.cpp
- * Author: jjones3
+ * File:   toBinary.cpp
+ * Author: jjones
  *
  * Created on September 14, 2018, 4:18 PM
  */
@@ -16,28 +10,26 @@
 #include <iostream>
 #include <sstream>
 
+using std::string;
 using namespace std;
-string toBinary(int decimal, string r = " ");
-int decimal; 
 
-/*
- * 
- */
+string toBinary(int decimal, string binary = "");
+
 int main(int argc, char** argv) {
-    
-    cout << "Please enter a number you wish to translate to binary: " ;
+    int decimal;
+
+    cout << "Please enter a number you wish to translate to binary: ";
     cin >> decimal;
-    cout << decimal;
-    toBinary(decimal);   
+
+    cout << toBinary(decimal) << endl; 
 
     return 0;
-}
+};
 
-string toBinary(int decimal, string r){
+string toBinary(int decimal, string binary){
     if (decimal < 1) {
-            return r;
+        return binary;
     }
-    else {
-        toBinary(decimal/2, r = (std::to_string(decimal%2) + r));
-    }
-}
+
+    return toBinary(decimal/2, std::to_string(decimal%2) + binary);
+};
